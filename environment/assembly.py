ab0bcddf037d3f4d43c86865593b9d9c8f8ff0b1
Youngin-Cho <56374406+Youngin-Cho@users.noreply.github.com>
2020-07-14 10:39:36 +0900

    DES modeling was added to assembly.py

--- environment/assembly.py
+++ environment/assembly.py
@@ -7,14 +7,14 @@ import numpy as np
 
 class Assembly(object):
     def __init__(self, num_of_processes, len_of_queue, inbound_panel_blocks=None, display_env=False):
-        self.env, self.model = self.DES_modeling(num_of_processes)
+        self.env, self.model = self._modeling(num_of_processes)
         self.num_of_processes = num_of_processes
         self.len_of_queue = len_of_queue
         self.action_space = len_of_queue
         self.observation_space = num_of_processes * len_of_queue + num_of_processes
         self.inbound_panel_blocks = inbound_panel_blocks
         self.inbound_panel_blocks_clone = self.inbound_panel_blocks[:]
-        self.processes = []
+        self.process = []
         self.queue = []
         self.stage = 0
         self.empty = -1
@@ -22,17 +22,20 @@ class Assembly(object):
             display = AssemblyDisplay()
             display.game_loop_from_space()
 
-    def DES_modeling(self, num_of_processes):
-        from environment.SimComponents import Source, Sink, Process
-        env = simpy.Environment()
-        model = []
-        Source = Source(env, 'Source', )
-        Sink = Sink(env, 'Sink')
-        model = [Source, Sink]
-        return env, model
-
     def step(self, action):
-        pass
+        done = False
+        block = self.queue.pop(action)
+        self.model[0].put(block)
+        while True:
+            self.env.step()
+            if self.model[0].server_num - self.model[0].server.count(None) > 0:
+                break
+        if len(self.queue) == 0:
+            done = True
+            self.env.run()
+        next_state = self._get_state()
+        reward = self._calculate_reward()
+        return next_state, reward, done
 
     def reset(self):
         self.inbound_panel_blocks = self.inbound_panel_blocks_clone[:]
@@ -47,12 +50,29 @@ class Assembly(object):
             for i in range(num):
                 panel_block = self.inbound_panel_blocks.pop()
                 self.queue.append(panel_block)
-        for i, process in enumerate(self.processes):
-
+        state[:self.num_of_processes] = self.process
+        
+        for i, process in enumerate(self.process):
+            pass
         for i, panel_block in enumerate(self.queue):
+            pass
+        return state
 
     def _calculate_reward(self):
-        pass
+        pass  # 이다원이 후처리 파일 만들고 나면 만들 것
+
+    def _modeling(self, num_of_processes):
+        from environment.SimComponents import Process, Sink
+        env = simpy.Environment()
+        model = []
+        processes = {}
+        for i in range(num_of_processes + 1):
+            model.append(Process(env, 'Process{0}'.format(i), processes, 1, qlimit=1))
+            processes['Process{0}'.format(i)] = model[-1]
+            if i == num_of_processes:
+                model.append(Sink(env, 'Sink'))
+                processes['Sink'] = model[-1]
+        return env, model
 
 
 class AssemblyDisplay(object):
