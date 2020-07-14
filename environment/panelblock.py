703c84f6757a0e6e898c1c073205df1862427272
SohyunNam <56374318+SohyunNam@users.noreply.github.com>
2020-07-14 14:01:22 +0900

    PanelBlock 변수명 수정

--- environment/panelblock.py
+++ environment/panelblock.py
@@ -13,18 +13,17 @@ def import_panel_block_schedule(filepath):
 
 
 class PanelBlock(object):
-    def __init__(self, data):
+    def __init__(self, panel_block):
         # block_id
-        self.id = data['product']
+        self.id = panel_block['product']
         # 작업 시간 저장
-        data.drop(['product', 'unit_assy'], inplace=True)
-        idx_list = data.index
-        index = pd.MultiIndex.from_product([[i for i in range(len(idx_list)+1)], ['start_time', 'process_time', 'process']])
-        self.panel_block = pd.Series(index=index)
-        for i, process in enumerate(idx_list):
-            self.panel_block[(i, 'process_time')] = data[process]
-            self.panel_block[(i, 'process')] = process
-        self.panel_block[len(idx_list), 'process'] = 'Sink'
+        panel_block.drop(['product', 'unit_assy'], inplace=True)
+        index = pd.MultiIndex.from_product([[i for i in range(len(panel_block.index)+1)], ['start_time', 'process_time', 'process']])
+        self.data = pd.Series(index=index)
+        for i, process in enumerate(panel_block.index):
+            self.data[(i, 'process_time')] = panel_block[process]
+            self.data[(i, 'process')] = 'Process{0}'.format(i)
+        self.data[len(panel_block.index), 'process'] = 'Sink'
         # 지나간 공정 수
         self.step = 0
 
