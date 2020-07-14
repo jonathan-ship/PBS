555aac266446a3145bfeba95e6392fd6035149b1
SohyunNam <56374318+SohyunNam@users.noreply.github.com>
2020-07-14 10:34:48 +0900

    PanelBlock class 생성

--- environment/panelblock.py
+++ environment/panelblock.py
@@ -5,23 +5,37 @@ import numpy as np
 def import_panel_block_schedule(filepath):
     df_schedule = pd.read_csv(filepath, encoding='euc-kr')
     panel_blocks = []
-    for i, row in df_schedule.iterrows():
-        panel_block = PanelBock(row['product'], row['plate_weld'], row['saw_front'], row['turn_over'], row['saw_back'],
-                                row['longi_weld'], row['unit_assy'], row['sub_assy'])
+    for i in range(len(df_schedule)):
+        temp = df_schedule.iloc[i]
+        panel_block = PanelBlock(temp)
         panel_blocks.append(panel_block)
     return panel_blocks
 
 
-class PanelBock(object):
-    def __init__(self, block_id=None, plate_weld=0, saw_front=0, turn_over=0, saw_back=0, longi_weld=0, unit_assy=0, sub_assy=0):
-        self.id = str(block_id)
-        self.plate_weld = plate_weld
-        self.saw_front = saw_front
-        self.turn_over = turn_over
-        self.saw_back = saw_back
-        self.longi_weld = longi_weld
-        self.unit_assy = unit_assy
-        self.sub_assy = sub_assy
+class PanelBlock(object):
+    def __init__(self, data):
+        # block_id
+        self.id = data['product']
+        # 작업 시간 저장
+        data.drop(['product', 'unit_assy'], inplace=True)
+        idx_list = data.index
+        index = pd.MultiIndex.from_product([[i for i in range(len(idx_list)+1)], ['start_time', 'process_time', 'process']])
+        self.panel_block = pd.Series(index=index)
+        for i, process in enumerate(idx_list):
+            self.panel_block[(i, 'process_time')] = data[process]
+            self.panel_block[(i, 'process')] = process
+        self.panel_block[len(idx_list), 'process'] = 'Sink'
+        # 지나간 공정 수
+        self.step = 0
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
